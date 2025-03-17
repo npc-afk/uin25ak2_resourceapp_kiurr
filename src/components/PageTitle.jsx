b75c5cc1c9e1280caf86e68b3fd7d9e5d2e69032
KiurR <polpen6@gmail.com>
2025-03-17 01:44:22 +0100

    final edits

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,3 +1,11 @@
-export default function PageTitle () {
-    return
-}
+import { useEffect } from "react";
+
+const PageTitle = ({ title }) => {
+  useEffect(() => {
+    document.title = `${title} | Ressursarkiv`;
+  }, [title]);
+
+  return <h2>{title}</h2>;
+};
+
+export default PageTitle;
