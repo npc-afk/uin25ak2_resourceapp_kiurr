134dc10b65b4d68a48e443676184144ad4f61670
KiurR <polpen6@gmail.com>
2025-03-17 01:37:05 +0100

    tror jeg er ferdig
    lagt til media queries

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,31 +1,26 @@
 import resources from "../ressurser";
 
 const Resources = ({ category }) => {
-    const filteredResources = resources.filter(resource => resource.category === category);
+  const filteredResources = resources.filter(resource => resource.category === category);
 
-    if (!filteredResources || filteredResources.length === 0) {
-        return (
-          <main>
-            <h2>{category ? category.toUpperCase() : "Ingen kategori valgt"} Ressurser</h2>
-            <p>Ingen ressurser funnet</p>
-          </main>
-        );
-      }
-    
-      return (
-        <main>
-          <h2>{category.toUpperCase()} Ressurser</h2>
-          <ul>
-            {filteredResources.map((resource, index) => (
-              <li key={index}>
-                <a href={resource.url} target="_blank" rel="noopener noreferrer">
-                  {resource.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </main>
-      );
-    };
-    
-    export default Resources;
+  return (
+    <main>
+      <h2>{category ? category.toUpperCase() : "Ingen kategori valgt"} Ressurser</h2>
+      {filteredResources.length > 0 ? (
+        <ul>
+          {filteredResources.map((resource, index) => (
+            <li key={index}>
+              <a href={resource.url} target="_blank" rel="noopener noreferrer">
+                {resource.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Ingen ressurser funnet</p> /*I bare noe ekstra jeg la til, synes det var lurt*/
+      )}
+    </main>
+  );
+};
+
+export default Resources;
